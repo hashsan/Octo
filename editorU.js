6f78027a4cd7344255ac6abe726e5689ce872dfe
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2023-11-14 13:51:55 +0900

    Create editorU.js

---
+++ editorU.js
@@ -0,0 +1,56 @@
+function clearurl(url){  
+ const {origin,pathname} = new URL(url)
+ return origin + pathname  
+}
+function getGhp(){
+  var d = "ghp_"
+  /**/
+  + "9ah8c3yojjO"
+  + "EsWBOP6CSiMAMj"
+  + "mcDcF1UGrhv"    
+  return d;
+}
+function isGithub(url){
+  return /\.github\.io/.test(url)
+}
+function getUrl(){
+  const def ='https://hashsan.github.io/outputs/cache.html'
+  var u = clearurl(location.href)
+  u = isGithub(u)?u:def 
+  u=u.replace(/.html$/,'.txt')
+  return u
+}
+
+
+async function editorU(){
+  const ed = document.querySelector('[contenteditable]')
+  if(!ed){    
+    return console.log('contenteditable not found') 
+  }
+  
+  const url = getUrl()    
+  const ghp = getGhp()  
+  const {Octo} = await import('https://hashsan.github.io/Octo/Octo.js');
+  const api = new Octo(url,ghp)
+  const img = await api.auth()
+  if(!img){
+    console.log(url,ghp)
+    throw new Error('Octo error')
+  }
+  
+  console.log('boot editorU',ed,url)
+  ed.innerText = await api.load()
+  
+  const {Bar} = await import('https://hashsan.github.io/Bar/Bar.js')
+  var bar = new Bar()
+  ed.addEventListener('keydown',async(e)=>{
+    if(e.ctrlKey && e.which===83){
+      e.preventDefault()
+      await api.save(ed.innerText)
+      bar.go(100)
+    }
+    bar.go(20)
+  })
+}
+
+editorU() ///////////////////////////////////////////////////////////////
